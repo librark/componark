9a2957776610d70b058f4878f1fa9866fbe93e08
David Narvaez <fdnarvaez@servagro.com.co>
2019-03-08 08:58:40 -0500

    list, item test 100%

--- src/components/list/components/list.js
+++ src/components/list/components/list.js
@@ -1,10 +1,5 @@
 export class List extends HTMLElement {
   connectedCallback () {
-    // this.render()
-  }
-
-  render () {
-    this.innerHTML = /* html */``
   }
 }
 customElements.define('ark-list', List)
---
+++ tests/components/button/list.spec.js
@@ -0,0 +1,8 @@
+import '../../../src/components/list'
+
+describe('List', () => {
+  it('can be instantiated', () => {
+    const list = document.createElement('ark-list')
+    expect(list).toBeTruthy()
+  })
+})
