5bfd2b87e1661b03f54364b193f33fe6473c7ba8
David Narvaez <fdnarvaez@servagro.com.co>
2019-03-13 14:11:34 -0500

    tabs style

--- src/components/tabs/components/tabs.js
+++ src/components/tabs/components/tabs.js
@@ -1,2 +1,36 @@
-export class Tabs extends HTMLElement { }
+export class Tabs extends HTMLElement {
+  connectedCallback () {
+    this.render()
+  }
+
+  render () {
+    this._autoActiveElement()
+    this._listen()
+  }
+
+  _listen () {
+    const items = Array.from(this.querySelectorAll('ark-tabs-item'))
+    items.forEach(i => i.addEventListener('click', _ =>
+      this._activeElement(i.firstElementChild))
+    )
+  }
+
+  _activeElement (element) {
+    const items = Array.from(this.querySelectorAll('ark-tabs-item'))
+    items.forEach(i => i.firstElementChild.removeAttribute('active'))
+
+    const att = document.createAttribute('active')
+    element.setAttributeNode(att)
+  }
+
+  _autoActiveElement () {
+    const items = Array.from(this.querySelectorAll('ark-tabs-item'))
+    var isActive = false
+    items.forEach(i => {
+      const att = Array.from(i.attributes)
+      isActive = isActive ? true : att.filter(a => a.name === 'active').length
+    })
+    if (!isActive) this._activeElement(items[0])
+  }
+}
 customElements.define('ark-tabs', Tabs)
