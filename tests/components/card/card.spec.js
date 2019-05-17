c2c06d9af5cfc311bf75d7df33f43b42ec96c5f9
David Narvaez <fdnarvaez@servagro.com.co>
2019-05-17 08:57:49 -0500

    100% card component test

--- tests/components/card/card.spec.js
+++ tests/components/card/card.spec.js
@@ -12,8 +12,28 @@ describe('Card', () => {
   it('can be rendered without content', function () {
     const element = document.createElement('ark-card')
     element.connectedCallback()
+    expect(!element.innerHTML.trim().length).toBeTruthy()
+  })
 
-    // const elementElement = element.querySelector('card')
-    // expect(elementElement).toBeTruthy()
+  it('can be rendered with content', function () {
+    const element = document.createElement('ark-card')
+    element.innerHTML = /* html */`
+      <img src="" slot="media"/>
+      <span slot="title">title</span>
+      <span slot="subtitle">subtitle</span>
+    `
+    element.connectedCallback()
+
+    expect(element.querySelector(
+      'div.ark-card__media [slot="media"]'
+    )).toBeTruthy()
+
+    expect(element.querySelector(
+      'div.ark-card__header h3.ark-card__title [slot="title"]'
+    )).toBeTruthy()
+
+    expect(element.querySelector(
+      'div.ark-card__header span.ark-card__subtitle [slot="subtitle"]'
+    )).toBeTruthy()
   })
 })
