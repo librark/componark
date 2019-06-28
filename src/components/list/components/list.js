cd14b54644aaae8467d6de796fa6f7b2f036f7ab
eecheverry <eecheverry@nubark.com>
2019-06-28 10:06:58 -0500

    Make list receive a source function to show its data

--- src/components/list/components/list.js
+++ src/components/list/components/list.js
@@ -1,12 +1,47 @@
 import { Component } from '../../component'
 
 export class List extends Component {
+  /** @param {Object} context */
   init (context) {
+    this.source = context['source'] || null
+
+    /** @type {Object[]} */
+    this.items = []
+
     return super.init(context)
   }
 
   render () {
+    if (this.items && this.items.length) {
+      this.innerHTML = /* html */`
+      ${this._renderItems()}
+      `
+    }
     return super.render()
+  }
+
+  async load () {
+    if (!this.source) return
+
+    this.items = await this.source()
+    this.render()
+  }
+
+  _renderItems () {
+    return /* html */`
+    <ul>
+      ${this.items.map((item, index) => `
+        ${this._renderItem(item, index)}
+      `).join('')}
+    </ul>
+    `
+  }
+
+  /** @param {Object} item @param {number} index */
+  _renderItem (item, index) {
+    return /* html */`
+    <li>${item[Object.keys(item)[0]]}</li>
+    `
   }
 }
 customElements.define('ark-list', List)
