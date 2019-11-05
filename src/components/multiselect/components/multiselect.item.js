68b3bcc3e939280e9e1ae58f153b0a34ddafef43
David Narvaez <fdnarvaez@servagro.com.co>
2019-11-05 16:39:23 -0500

    style multiselect

--- src/components/multiselect/components/multiselect.item.js
+++ src/components/multiselect/components/multiselect.item.js
@@ -4,59 +4,59 @@ export class MultiselectItem extends Component {
 	/**
 	 * @param {{id, data,	template?}} context
 	 */
-	init(context) {
-		this.data = context['data'] || null
-		this.id = context['id'] || this.id
-		this.template = context['template'] || (data => `${data}`)
+  init(context) {
+    this.data = context['data'] || null
+    this.id = context['id'] || this.id
+    this.template = context['template'] || (data => `${data}`)
 
-		return super.init()
-	}
+    return super.init()
+  }
 
-	render() {
-		this.innerHTML = /* html */ `
-			<div>${this.template(this.data)}</div>
+  render() {
+    this.innerHTML = /* html */ `
+			<small>${this.template(this.data)}</small>
 			<button listen listen on-click="_onRemove" remove>&times;</button>
 		`
-		return super.render()
-	}
+    return super.render()
+  }
 
-	reflectedProperties() {
-		return ['id']
-	}
+  reflectedProperties() {
+    return ['id']
+  }
 
-	load() {
-		return super.load()
-	}
+  load() {
+    return super.load()
+  }
 
-	// ---------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
 
-	/** @param {boolean} value */
-	set selected(value) {
-		if (value) {
-			this.setAttribute('selected', 'true')
-		} else {
-			this.removeAttribute('selected')
-		}
-	}
+  /** @param {boolean} value */
+  set selected(value) {
+    if (value) {
+      this.setAttribute('selected', 'true')
+    } else {
+      this.removeAttribute('selected')
+    }
+  }
 
-	/** @return {boolean} */
-	get selected() {
-		return this.hasAttribute('selected')
-	}
+  /** @return {boolean} */
+  get selected() {
+    return this.hasAttribute('selected')
+  }
 
-	// ---------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
 
-	/** @param {event} event */
-	_onRemove(event) {
-		event.stopImmediatePropagation()
-		this.dispatchEvent(
-			new CustomEvent('multiselect-item:remove', {
-				bubbles: true,
-				detail: {
-					data: this.data
-				}
-			})
-		)
-	}
+  /** @param {event} event */
+  _onRemove(event) {
+    event.stopImmediatePropagation()
+    this.dispatchEvent(
+      new CustomEvent('multiselect-item:remove', {
+        bubbles: true,
+        detail: {
+          data: this.data
+        }
+      })
+    )
+  }
 }
 customElements.define('ark-multiselect-item', MultiselectItem)
