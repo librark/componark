b922792a9c80ea9d87fc138027b897b5542c411d
David Narvaez <fdnarvaez@servagro.com.co>
2019-12-18 09:23:16 -0500

    card style

--- src/showcase/screens/base/card/cardDemo.js
+++ src/showcase/screens/base/card/cardDemo.js
@@ -3,15 +3,15 @@ import { Component } from '../../loader'
 import Image from './assets/building.jpg'
 
 export class CardDemo extends Component {
-	init (context) {
-		this.type = context['type'] || 'ark'
-		return super.init(context)
-	}
+  init (context) {
+    this.type = context['type'] || 'ark'
+    return super.init(context)
+  }
 
-	render () {
-		this.innerHTML = /* html */ `
-      <ark-card title="titulo" subtitle="subtitle [Hello World]">
-        <img slot="media" src="${Image}" alt=""/>
+  render () {
+    this.innerHTML = /* html */ `${this.styles}
+    <ark-card title="titulo" subtitle="subtitle [Hello World]">
+        <img slot="media" src="${Image}" alt="" />
 
         <div>
           body
@@ -21,8 +21,17 @@ export class CardDemo extends Component {
         <ark-button slot="action">btn 2</ark-button>
       </ark-card>
     `
+    return super.render()
+  }
 
-		return super.render()
-	}
+  get styles () {
+    return /* html */`
+      <style>
+        demo-card{
+          padding: .5rem;
+        }
+      </style>
+    `
+  }
 }
 customElements.define('demo-card', CardDemo)
