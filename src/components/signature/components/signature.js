eb8b31749a0fd2e594adc09f908f2a9781a0b2d9
David Narvaez <fdnarvaez@servagro.com.co>
2020-02-07 09:50:54 -0500

    signature component

--- src/components/signature/components/signature.js
+++ src/components/signature/components/signature.js
@@ -1,3 +1,5 @@
+import * as SignaturePad from 'signature_pad/dist/signature_pad'
+
 import { Component } from '../../component'
 
 export class Signature extends Component {
@@ -7,13 +9,25 @@ export class Signature extends Component {
 
   render() {
     this.innerHTML = /* html */`
-      <canvas id="signature-pad" class="signature-pad" width=400 height=200></canvas>
+      <canvas data-signature-pad class="ark-signature--pad"
+        width=400 height=200></canvas>
     `
+
+    this.signaturePad = new SignaturePad(this.canvas, {
+      backgroundColor: 'rgba(255, 255, 255, 1)',
+      penColor: 'rgb(0, 0, 0)'
+    })
+
     return super.render()
   }
 
   load() {
     return super.load()
+  }
+
+  /** @returns {HTMLElement} */
+  get canvas() {
+    return this.querySelector("[data-signature-pad]")
   }
 }
 customElements.define('ark-signature', Signature)
