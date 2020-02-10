33ee72a5eeb0547ac462352d9013f3a1c8bc81b1
David Narvaez <fdnarvaez@servagro.com.co>
2020-02-10 09:42:25 -0500

    navigator.mediaDevices

--- Makefile
+++ Makefile
@@ -39,3 +39,6 @@ pull:
 
 http:
 	http-server -p 8080 -c-1 dist
+
+browser-sync:
+	browser-sync start --proxy "localhost:7890" --files "*"
--- src/components/camera/components/camera.js
+++ src/components/camera/components/camera.js
@@ -6,13 +6,37 @@ export class Camera extends Component {
   }
 
   render() {
-    this.innerHTML = "[[[camera]]]"
+    this.innerHTML = /* html */`
+      <video data-video playsinline autoplay></video>
+      <canvas data-canvas></canvas>
+
+      <button listen on-click="initWebcam">Take photo</button>
+    `
 
     return super.render()
   }
 
   load() {
     return super.load()
+  }
+
+  // Access webcam
+  initWebcam() {
+    var constraints = { video: true, audio: true }
+
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(stream => this.video.srcObject = stream)
+      .catch(e => console.error(e))
+  }
+
+  /** @returns {HTMLVideoElement} */
+  get video() {
+    return this.querySelector('[data-video]')
+  }
+
+  /** @returns {HTMLVideoElement} */
+  get canvas() {
+    return this.querySelector('[data-canvas]')
   }
 }
 customElements.define('ark-camera', Camera)
