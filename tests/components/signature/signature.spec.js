6944423098d3709759dc155d9b96b4899877aa79
David Narvaez <fdnarvaez@servagro.com.co>
2020-02-18 08:58:46 -0500

    signature test

--- tests/components/signature/signature.spec.js
+++ tests/components/signature/signature.spec.js
@@ -6,7 +6,21 @@ describe('Signature', () => {
     signature.init()
     signature.render()
     signature.load()
-    signature.getSrc()
+    signature.dataURL()
     signature.clear()
+    signature.resizeCanvas()
+    signature.disconnectedCallback()
+
+    expect(signature.querySelector('canvas')).toBeTruthy()
+  })
+
+  it('can be resize Canvas', () => {
+    const signature = new Signature()
+    signature.init()
+    signature.render()
+    signature['global'].devicePixelRatio = 0
+    signature.resizeCanvas()
+
+    expect(signature.querySelector('canvas')).toBeTruthy()
   })
 })
