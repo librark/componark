f294d3ee484a2862a5cdc980b12aa9b8ba81931f
fdnarvaez <fdnarvaez@servagro.com.co>
2020-10-05 09:45:40 -0500

    splitview

--- src/components/splitview/index.d.ts
+++ src/components/splitview/index.d.ts
@@ -4,3 +4,13 @@ export class SplitView extends Component {
   master: Component
   detail: Component
 }
+
+export class SplitViewMaster extends Component { }
+
+export class SplitViewDetail extends Component {
+  show ()
+
+  hide ()
+
+  toggle ()
+}
