0cce4e208322002d8511feaa318e564681976996
fdnarvaez <fdnarvaez@servagro.com.co>
2020-10-09 16:31:27 -0500

    package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    jasmine: true,
+    node: true
   },
   extends: [
     'standard'
   ],
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module'
   },
   rules: {
     'don-notation': false,
+    'no-tabs': 0,
+    "max-len": [
+      "error",
+      80,
+      2
+    ],
   }
 }
