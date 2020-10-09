05e5660b42f859491dc569b15a73d5c7ad871888
fdnarvaez <fdnarvaez@servagro.com.co>
2020-10-09 17:11:00 -0500

    tests

--- tests/components/tabs/tabs.item.spec.js
+++ tests/components/tabs/tabs.item.spec.js
@@ -1,16 +1,18 @@
-import { Tabs } from '../../../src/components/tabs'
+import { TabsItem } from '../../../src/components/tabs'
 
 describe('Tabs Item', () => {
   it('can be instantiated', () => {
-    const item = new Tabs()
+    const item = new TabsItem()
     expect(item).toBeTruthy()
 
     const init = item.init()
     expect(item === init).toBeTruthy()
+
+    init.init().render().load()
   })
 
   it('can be rendered with content', function () {
-    const item = new Tabs()
+    const item = new TabsItem()
     item.setAttribute('id', 'item-1')
 
     item.innerHTML = /* HTML */ `
@@ -21,8 +23,19 @@ describe('Tabs Item', () => {
     expect(item.getAttribute('id') === 'item-1').toBeTruthy()
   })
 
+  it('can be rendered with type', function () {
+    const tabItemTypeLink = new TabsItem()
+    tabItemTypeLink.setAttribute('href', 'http')
+    tabItemTypeLink.init().render().load()
+    expect(tabItemTypeLink.querySelector('a')).toBeTruthy()
+
+    const tabItemTypeButton = new TabsItem()
+    tabItemTypeButton.init().render().load()
+    expect(tabItemTypeButton.querySelector('button')).toBeTruthy()
+  })
+
   it('can remove attributes', function () {
-    const item = new Tabs()
+    const item = new TabsItem()
 
     item.setAttribute('name', 'my-item')
     item.setAttribute('id', 'it-1')
