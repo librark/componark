8de1aa18d61ebdf647942358a40d1e742cb0dc86
fdnarvaez <fdnarvaez@servagro.com.co>
2020-10-26 18:03:20 -0500

    pivot

--- src/components/pivot/components/pivot.js
+++ src/components/pivot/components/pivot.js
@@ -1,4 +1,5 @@
 import { Component } from '../../component'
+import WebDataRocks from 'webdatarocks'
 
 export class Pivot extends Component {
   init (context = {}) {
@@ -8,7 +9,21 @@ export class Pivot extends Component {
   render () {
     this.innerHTML = /* html */`
       <h1>Pivot</h1>
+
+      <div id="wdr-component"></div>
     `
+
+
+    var pivot = new WebDataRocks({
+      container: "#wdr-component",
+      toolbar: true,
+      report: {
+        dataSource: {
+          filename: "https://cdn.webdatarocks.com/data/data.csv"
+        }
+      }
+    })
+
     return super.render()
   }
 }
