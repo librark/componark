5fa7f4b60a17c77b9b8f51c8fa2b93ca26f68767
fdnarvaez <fdnarvaez@servagro.com.co>
2020-11-06 15:38:44 -0500

    tabulator

--- src/components/tabulator/components/tabulator.js
+++ src/components/tabulator/components/tabulator.js
@@ -4,13 +4,11 @@ import { Component } from '../../component'
 export class TabulatorTable extends Component {
   /**
    * @param {{
-   *  dataSource?: object
    *  options?: object
    * }} [context]
    **/
   init (context = {}) {
-    this.dataSource = context.dataSource || {}
-    this.options = context.options || {}
+    this.options = context || {}
     return super.init()
   }
 
@@ -25,18 +23,9 @@ export class TabulatorTable extends Component {
   }
 
   renderTabulatorTable () {
-    const content = this.querySelector("#tabulator-table")
-
-    var tabledata = [
-      { id: 1, name: "Billy Bob", age: 12, gender: "male", height: 95, col: "red", dob: "14/05/2010" },
-      { id: 2, name: "Jenny Jane", age: 42, gender: "female", height: 142, col: "blue", dob: "30/07/1954" },
-      { id: 3, name: "Steve McAlistaire", age: 35, gender: "male", height: 176, col: "green", dob: "04/11/1982" },
-    ]
-
-    this.table = new Tabulator(content, {
-      data: tabledata,
-      autoColumns: true,
-    })
+    const element = this.querySelector("#tabulator-table")
+    this.table = new Tabulator(element, this.options)
+    this.table.redraw()
   }
 }
 customElements.define('ark-tabulator', TabulatorTable)
--- src/showcase/screens/base/tabulator/tabulatorDemo.js
+++ src/showcase/screens/base/tabulator/tabulatorDemo.js
@@ -16,14 +16,20 @@ export class TabulatorDemo extends Component {
         <ark-tabulator></ark-tabulator>
       </div>
     `
-
-    // this.tabulator.init({
-    //   dataSource: {
-    //     filename: "https://cdn.webdatarocks.com/data/data.csv"
-    //   }
-    // }).render()
-
+    this.renderTabulator()
     return super.render()
+  }
+
+  renderTabulator () {
+    const data = [
+      { id: 1, name: "Billy Bob", age: 12, gender: "male", height: 95, col: "red", dob: "14/05/2010" },
+      { id: 2, name: "Jenny Jane", age: 42, gender: "female", height: 142, col: "blue", dob: "30/07/1954" },
+      { id: 3, name: "Steve McAlistaire", age: 35, gender: "male", height: 176, col: "green", dob: "04/11/1982" },
+    ]
+    this.tabulator.init({
+      data,
+      autoColumns: true,
+    }).render()
   }
 
   get tabulator () {
