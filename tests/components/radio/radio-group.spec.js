3eb1e492eece6e6cb80f29ceb266ef19c25080ae
cristianV12 <cristianvelasco0312@gmail.com>
2021-02-04 19:03:57 -0500

    test radio group

--- tests/components/radio/radio-group.spec.js
+++ tests/components/radio/radio-group.spec.js
@@ -7,16 +7,97 @@ describe('RadioGroup', () => {
     container = document.createElement('div')
     document.body.appendChild(container)
   })
-
+  
   afterEach(() => {
     container.remove()
     container = null
   })
-
+  
   it('can be instantiated', () => {
+    container.innerHTML = /* html */ `
+    <ark-radio-group></ark-radio-group>`
+    
+    const radioGroup = container.querySelector('ark-radio-group')
+    expect(radioGroup).toBeTruthy
+    expect(radioGroup).toBe(radioGroup.init())
+  })
+
+  it('can be instantiated with label attribute', () => {
+    container.innerHTML = /* html */ `
+    <ark-radio-group label="my group"></ark-radio-group>
+    `
+    const element = container.querySelector('ark-radio-group')
+    const label = element.querySelector('[data-radio-group-label]')
+
+    expect(label.textContent.trim()).toEqual('my group')
+    expect(!element.value.trim().length).toBeTruthy()
+  })
+
+  it('returns selected values', () => {
+    container.innerHTML = /* html */ `
+      <ark-radio-group>
+        <ark-radio-button value="op1">Option 1</ark-radio-button>
+        <ark-radio-button value="op2">Option 2</ark-radio-button>
+      </ark-radio-group>
+    `
+
+    const group = container.querySelector('ark-radio-group')
+    const radioButtons = container.querySelectorAll('ark-radio-button')
+
+    const radio1 = radioButtons[0]
+    expect(radio1.value).toBe('op1')
+    
+    const radio2 = radioButtons[1]
+    expect(radio2.value).toBe('op2')
+    // @ts-ignore
+    expect(!group.value.trim().length).toBeTruthy()
+  })
+
+   it('returns selected values Group', () => {
+    container.innerHTML = /* html */ `
+      <ark-radio-group>
+        <ark-radio-button value="op1">Option 1</ark-radio-button>
+        <ark-radio-button value="op2">Option 2</ark-radio-button>
+        <ark-radio-button value="op3">Option 3</ark-radio-button>
+      </ark-radio-group>
+    `
+
+    const radioGroup = container.querySelector('ark-radio-group')
+    
+    const radioButtons = container.querySelectorAll('ark-radio-button')
+    
+    const radio0 = /** @type {RadioButton} */ (
+      radioButtons[0]
+      )
+    const radio1 = /** @type {RadioButton} */ (
+      radioButtons[1]
+      )
+    const radio2 = /** @type {RadioButton} */ (
+      radioButtons[2]
+      )
+          
+          
+    radio0.click()
+    expect(radioGroup.value).toEqual('op1')
+    expect(radio0.checked).toBeTruthy
+    
+    radio1.click()
+    expect(radioGroup.value).toEqual('op2')
+    expect(radio1.checked).toBeTruthy
+    
+    radio2.click()
+    expect(radioGroup.value).toEqual('op3')
+    expect(radio2.checked).toBeTruthy
+  
+  })
+
+
+
+  
+  xit('can be instantiated', () => {
 
     container.innerHTML = /* html */ `
-        <ark-radio-group label="Radio Buttons">
+        <ark-radio-group label="Radio Button">
           <ark-radio-button value="op1">Option 1</ark-radio-button>
           <ark-radio-button value="op2">Option 2</ark-radio-button>
           <ark-radio-button value="op3">Option 3</ark-radio-button>
@@ -52,67 +133,8 @@ describe('RadioGroup', () => {
     radio1.click()
   })
 
-  it('can be instantiated', () => {
-    container.innerHTML = /* html */ `
-    <ark-radio-group label="Radio Buttons">
-      <ark-radio-button value="op1">Option 1</ark-radio-button>
-      <ark-radio-button value="op2">Option 2</ark-radio-button>
-      <ark-radio-button value="op3">Option 3</ark-radio-button>
-    </ark-radio-group>
-    `
 
-    const element = container.querySelector('ark-radio-group')
-    expect(element).toBeTruthy()
-
-    const init = element.init({})
-    expect(element === init).toBeTruthy()
-  })
-
-  it('can be instantiated', () => {
-    container.innerHTML = /* html */ `
-    <ark-radio-group label="Radio Buttons">
-      <ark-radio-button value="op1">Option 1</ark-radio-button>
-      <ark-radio-button value="op2">Option 2</ark-radio-button>
-      <ark-radio-button value="op3">Option 3</ark-radio-button>
-    </ark-radio-group>
-    `
-    const element = container.querySelector('ark-radio-group')
-    element.setAttribute('label', 'my group')
-    element.connectedCallback()
-
-    const label = element.querySelector('[data-radio-group-label]')
-    expect(label.textContent.trim()).toEqual('my group')
-    expect(!element.value.trim().length).toBeTruthy()
-  })
-
-  it('returns selected values', () => {
-    container.innerHTML = /* html */ `
-      <ark-radio-group label="Radio Buttons">
-        <ark-radio-button value="op1">Option 1</ark-radio-button>
-        <ark-radio-button value="op2">Option 2</ark-radio-button>
-        <ark-radio-button value="op3">Option 3</ark-radio-button>
-      </ark-radio-group>
-    `
-
-    const group = container.querySelector('ark-radio-group')
-    const radioButtons = container.querySelectorAll('ark-radio-button')
-
-    const radio1 = radioButtons[0]
-    radio1.value = 'op1'
-
-    const radio2 = radioButtons[1]
-    radio2.value = 'op2'
-
-    // @ts-ignore
-    group.defaultContent = null
-    group.appendChild(radio1)
-    group.appendChild(radio2)
-    group.load()
-
-    expect(!group.value.trim().length).toBeTruthy()
-  })
-
-  it('returns selected values Group', () => {
+  xit('returns selected values Group', () => {
     container.innerHTML = /* html */ `
       <ark-radio-group label="Radio Buttons">
         <ark-radio-button value="op1">Option 1</ark-radio-button>
@@ -157,4 +179,7 @@ describe('RadioGroup', () => {
 
     radioGroup.click()
   })
+  
+ 
 })
+  
