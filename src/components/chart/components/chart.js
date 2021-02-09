f16d17a10130b3a5713f727d55185f3c78bbe015
cristianV12 <cristianvelasco0312@gmail.com>
2021-02-09 16:05:21 -0500

    Chart styles added

--- src/components/chart/components/chart.js
+++ src/components/chart/components/chart.js
@@ -1,6 +1,8 @@
 import { Chart as ChartJs } from 'chart.js'
 import { Component } from '../../../base/component'
+import { styles } from '../styles'
 
+const tag = 'ark-chart'
 export class Chart extends Component {
   init (context = {}) {
     this.details = context.details
@@ -81,4 +83,4 @@ export class Chart extends Component {
     return `rgba(${r},${g},${b},${alpha})`
   }
 }
-customElements.define('ark-chart', Chart)
+Component.define(tag, Chart, styles)
