dc1a53b4e6d46d88a56e3596f27ca03b13db4233
subcorporeal <subcorporeal@gmail.com>
2021-04-22 17:13:09 -0500

    input file preview and capture

--- src/components/droparea/components/droparea.js
+++ src/components/droparea/components/droparea.js
@@ -1,15 +1,15 @@
-import { Component } from '../../../base/component'
-import {styles} from '../styles'
+import { Component } from "../../../base/component"
+import { styles } from "../styles"
 // @ts-ignore
-const tag = 'ark-droparea'
+const tag = "ark-droparea"
 
 export class Droparea extends Component {
-  init (context = {}) {
-        return super.init()
+  init(context = {}) {
+    return super.init()
   }
 
-  render () {
-    this.content = /* html */`
+  render() {
+    this.content = /* html */ `
       <form class="ark-droparea__form">
           <ark-icon type="mat" name="cloud_upload"></ark-icon>
           <h1 class="ark-droparea__message">
@@ -19,124 +19,121 @@ export class Droparea extends Component {
                class="ark-droparea__input"
                id="fileElem" 
                multiple 
-               accept="image/*" 
-               onchange="handleFiles(this.files)">
+                >
         <div class="ark-droparea__gallery"></div>
       </form>
   `
-    
-      this.dragDropEvents =['dragenter','dragover','dragleave','drop']
-      this.dragEvents = this.dragDropEvents.slice(0,2)
-      this.dropEvents = this.dragDropEvents.slice(2)
+    this.dragDropEvents = ["dragenter", "dragover", "dragleave", "drop"]
+    this.dragEvents = this.dragDropEvents.slice(0, 2)
+    this.dropEvents = this.dragDropEvents.slice(2)
+    this._input = this.select(".ark-droparea__input")
 
-    /* Progress bar variables */
-    //<progress max="100" value="0" class="ark-droparea__progress"></progress>
-
-    // this.filesDone = 0
-    // this.filesToDo = 0
+    this.fileList = []
     return super.render()
-    }
-    
-    async load () {
-        this.dragDropEvents.forEach(eventName => {
-            this.addEventListener(eventName, this.preventDefaults, false)
-        })
-      
-        this.dragEvents.forEach(eventName => {
-            this.addEventListener(eventName, this.highlight, false)
-        })
-    
-        this.dropEvents.forEach(eventName => {
-            this.addEventListener(eventName, this.unhighlight, false)
-        })
-    
-        // this.addEventListener('drop', this.handleDrop, false)
   }
 
-    preventDefaults (e) {
-        e.preventDefault()
-        e.stopPropagation()
+  async load() {
+    this.dragDropEvents.forEach((eventName) => {
+      this.addEventListener(eventName, this.preventDefaults, false)
+    })
+
+    this.dragEvents.forEach((eventName) => {
+      this.addEventListener(eventName, this.highlight, false)
+    })
+
+    this.dropEvents.forEach((eventName) => {
+      this.addEventListener(eventName, this.unhighlight, false)
+    })
+
+    this.addEventListener("drop", this.handleDrop, false)
+    this._input.addEventListener("change", this.onChange, false)
+  }
+
+  preventDefaults(e) {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+
+  highlight(e) {
+    this.dropZone.classList.add("highlight")
+  }
+
+  unhighlight(e) {
+    this.dropZone.classList.remove("highlight")
+  }
+
+  handleDrop(e) {
+    let data = e.dataTransfer
+    let files = data.files
+    this.handleFiles(files)
+    console.log(this.returnFiles)
+  }
+
+  onChange(e) {
+    const droparea = this.parentElement.parentElement
+    const input = e.target
+    const files = input.files
+    droparea.handleFiles(files)
+  }
+
+  handleFiles(files) {
+    files = [...files]
+    // files.forEach(this.uploadFile)
+    files.forEach(this.previewFile)
+    files.forEach((file) => this.fileList.push(file))
+  }
+
+  // uploadFile(file){
+  //     let url = 'YOUR URL HERE'
+  //     let formData = new FormData()
+
+  //     formData.append('file', file)
+
+  //     fetch(url,{
+  //         method: 'POST',
+  //         body: formData
+  //     })
+  //         .then(()=>{
+  //             /* Done. Inform the user*/
+  //             //this.progressDone
+  //         })
+  //         .catch(()=>{
+  //             /* Error, Inform the user */
+  //         })
+  // }
+
+  previewFile(file) {
+    const gallery = document.querySelector(".ark-droparea__gallery")
+    let reader = new FileReader()
+    let fileType = file.type.split("/")[0]
+    reader.readAsDataURL(file)
+    reader.onloadend = () => {
+      let picture = document.createElement("picture")
+      let p = document.createElement("p")
+      p.innerText = file.name
+      picture.style.backgroundImage = `url('${reader.result}')`
+      fileType != "image"
+        ? gallery.appendChild(p)
+        : gallery.appendChild(picture)
     }
+  }
 
-    highlight (e){
-        this.dropZone.classList.add('highlight')
-    }
-    
-    unhighlight(e){
-        this.dropZone.classList.remove('highlight')
-    }
-    
-    
-    // handleDrop(e){
-    //     let data = e.dataTransfer
-    //     let files = data.files
-    //     this.handleFiles(files)
-    // }
-    
-    // handleFiles(files){
-    //     files = [...files]
-    //     // this.initializeProgress(files.lenght)
-    //     // files.forEach(this.uploadFile)
-    //     files.forEach(this.previewFile)
-        
-    //     // let formData =  new FormData
-    //     // for(let file in files){
-    //     //     formData.append('file', files[file])
-    //     // }
-    //     // let fileList = formData.getAll('file')
-    //     // return fileList
-    // }
-    
-    // uploadFile(file){  
-    //     let url = 'YOUR URL HERE'
-    //     let formData = new FormData()
+  // printRawData() {
+  //   const myFiles = this.returnFiles
+  //   const reader = new FileReader()
 
-    //     formData.append('file', file)
+  //   reader.readAsBinaryString(myFiles[0])
 
-    //     fetch(url,{
-    //         method: 'POST',
-    //         body: formData
-    //     })
-    //         .then(()=>{
-    //             /* Done. Inform the user*/ 
-    //             //this.progressDone
-    //         })
-    //         .catch(()=>{
-    //             /* Error, Inform the user */
-    //         })
-    // }
+  //   reader.onloadend = () => {
+  //     console.log(reader.result)
+  //   }
+  // }
+  get dropZone() {
+    return this.select(".ark-droparea__form")
+  }
 
-    // previewFile(file){
-    //     const gallery = document.querySelector('.ark-droparea__gallery')
-    //     let reader = new FileReader()
-    //     let fileType = file.type.split('/')[0]
-    //     reader.readAsDataURL(file)
-    //     reader.onloadend = ()=>{
-    //         let picture = document.createElement('picture')
-    //         let p = document.createElement('p')
-    //         p.innerText = file.name
-    //         picture.style.backgroundImage = `url('${reader.result}')`
-    //         fileType != 'image' ? gallery.appendChild(p) : gallery.appendChild(picture)
-    //     }    
-    // }    
-
-    // initializeProgress(numFiles){
-    //     this.progressBar.value = 0
-    //     this.filesDone = 0
-    //     this.filesToDo = numFiles
-    // }
-
-    // progressDone(){
-    //     this.filesDone++
-    //     this.progressBar.value = this.filesDone / this.filesToDo * 100
-    // }
-
-    // get progressBar () {
-    //     return this.select('.ark-droparea__progress')
-    // }
-
-    get dropZone(){
-        return this.select('.ark-droparea__form')
-    }
+  get returnFiles() {
+    return this.fileList
+  }
 }
 Component.define(tag, Droparea, styles)
--- tests/components/droparea/droparea.spec.js
+++ tests/components/droparea/droparea.spec.js
@@ -1,74 +1,99 @@
-import { Droparea } from 'components/droparea'
+import { Droparea } from "components/droparea"
 
-describe('Droparea', () => {
-    const file = new File([""], "myimage.png", {
-        type: "image/png"
+describe("Droparea", () => {
+
+  const createBubbledEvent = (type, props = {}) => {
+    const event = new Event(type, { bubbles: true })
+    Object.assign(event, props)
+    return event
+  }
+
+  let container = null
+
+  beforeEach(() => {
+    container = document.createElement("div")
+    document.body.appendChild(container)
+  })
+
+  afterEach(() => {
+    container.remove()
+    container = null
+  })
+
+  it("can be instantiated", () => {
+    container.innerHTML = /* html */ `
+            <ark-droparea></ark-droparea>
+        `
+
+    const droparea = container.querySelector("ark-droparea")
+    expect(droparea).toBe(droparea.init())
+  })
+
+  it("drag files to zone highlights the drop area", () => {
+    container.innerHTML = /* html */ `
+            <ark-droparea></ark-droparea>
+        `
+
+    const droparea = container.querySelector("ark-droparea")
+    const dropZone = droparea.dropZone
+    const dragNode = dropZone
+
+    dragNode.dispatchEvent(
+      createBubbledEvent("dragover", { clientX: 0, clientY: 1 })
+    )
+    expect(dropZone.classList["1"]).toBe("highlight")
+  })
+
+  it("drag files outside the zone unhighlight the drop area", () => {
+    container.innerHTML = /* html */ `
+            <ark-droparea></ark-droparea>
+        `
+
+    const droparea = container.querySelector("ark-droparea")
+    const dropZone = droparea.dropZone
+
+    dropZone.dispatchEvent(
+      createBubbledEvent("dragleave", { clientX: 0, clientY: 1 })
+    )
+    expect(dropZone.classList.length).toBe(1)
+  })
+
+  it("temporal test", () => {
+    container.innerHTML = /* html */ `
+            <ark-droparea></ark-droparea>
+        `
+
+    const droparea = container.querySelector("ark-droparea")
+    const dropZone = droparea.querySelector('.ark-droparea__form')
+    const dropGallery = droparea.querySelector(".ark-droparea__gallery")
+    const myFile = new File(["image"], "myimage.png", {
+      type: "image/png",
     })
 
-    // const mockDragEvent = new window.Event("dragenter")
-    // Object.defineProperty(event,"dataTransfer",{
-    //     value: {
-    //         files: [file], 
-    //     }
-    // })
-    const createBubbledEvent = (type, props = {}) =>{
-        const event = new Event(type, {bubbles: true})
-        Object.assign(event,props)
-        return event
+    const testStorage = new Map();
+    const testEvent = {
+      dataTransfer:{ 
+        setData: (key, value) =>testStorage.set(key, value),
+        getData: (key) => testStorage.get(key)
+      }
     }
+    spyOn(testEvent.dataTransfer, 'getData').and.callThrough();
+    const dropEvent = createBubbledEvent("drop", {
+        clientX: 0,
+        clientY: 1,
+        dataTransfer: testEvent.dataTransfer,
+      })
 
-   let container = null;
-
-    
-    beforeEach(() => {
-        container = document.createElement('div')
-        document.body.appendChild(container)
+      
+      Object.defineProperty(dropEvent, "dataTransfer", {
+        value: {
+          files: [myFile]
+        }
+      })
+      
+      dropZone.dispatchEvent(dropEvent)
+      droparea.addEventListener('click',droparea.previewFile(myFile))
+      droparea.click()
+      droparea.returnFiles()
     })
-
-    afterEach(() => {
-        container.remove()
-        container = null
-    })
-
-    it('can be instantiated', () => {
-        container.innerHTML = /* html */ `
-            <ark-droparea></ark-droparea>
-        `
-
-        const droparea= container.querySelector('ark-droparea')
-        expect(droparea).toBe(droparea.init())
-    })
-
-    it('drag files to zone highlights the drop area', () => {
-        container.innerHTML = /* html */ `
-            <ark-droparea></ark-droparea>
-        `
-   
-        const droparea= container.querySelector('ark-droparea')
-        const dropZone= droparea.dropZone
-        const dragNode = dropZone
-        
-        dragNode.dispatchEvent(
-            createBubbledEvent("dragover", {clientX:0, clientY:1})
-        )
-        expect(dropZone.classList['1']).toBe('highlight')
-    })
-
-     it('drag files outside the zone unhighlight the drop area', () => {
-        container.innerHTML = /* html */ `
-            <ark-droparea></ark-droparea>
-        `
-   
-        const droparea= container.querySelector('ark-droparea')
-        const dropZone= droparea.dropZone
-        const dragNode = dropZone
-        
-        dragNode.dispatchEvent(
-            createBubbledEvent("dragleave", {clientX:0, clientY:1})
-        )
-        expect(dropZone.classList.length).toBe(1)
-    })
-
-
-
 })
