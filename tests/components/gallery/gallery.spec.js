8ef9336d97d79e3f23393fe782065c12fb115c02
subcorporeal <subcorporeal@gmail.com>
2021-08-20 12:18:21 -0500

    gallery refactor

--- tests/components/gallery/gallery.spec.js
+++ tests/components/gallery/gallery.spec.js
@@ -1,10 +1,10 @@
-import { Gallery } from "components/gallery"
+import { Gallery } from 'components/gallery'
 
-describe("Gallery", () => {
+describe('Gallery', () => {
   let container = null
 
   beforeEach(() => {
-    container = document.createElement("div")
+    container = document.createElement('div')
     document.body.appendChild(container)
   })
 
@@ -13,16 +13,16 @@ describe("Gallery", () => {
     container = null
   })
 
-  it("can be instantiated", () => {
+  it('can be instantiated', () => {
     container.innerHTML = /* html */ `
     <ark-gallery></ark-gallery>`
 
-    const gallery = container.querySelector("ark-gallery")
+    const gallery = container.querySelector('ark-gallery')
 
     expect(gallery).toBe(gallery.init())
   })
 
-  it("image thumbnails can be rendered", () => {
+  it('image thumbnails can be rendered', () => {
     container.innerHTML = /* html */ `
     <ark-gallery images="https://picsum.photos/200/300?random=1| 
                          https://picsum.photos/200/300?random=2| 
@@ -30,45 +30,42 @@ describe("Gallery", () => {
     </ark-gallery>
     `
 
-    const gallery = container.querySelector("ark-gallery")
-    const li = gallery.querySelectorAll("li")
+    const gallery = container.querySelector('ark-gallery')
+    const li = gallery.querySelectorAll('li')
 
     expect(li.length).toBe(gallery.imageList.length)
   })
 
-  it("image thumbnails can be selected", () => {
+  it('image thumbnails can be selected', () => {
     container.innerHTML = /* html */ `
-    <ark-gallery images="https://picsum.photos/200/300?random=1| 
-                         https://picsum.photos/200/300?random=2| 
-                         https://picsum.photos/200/300?random=3">
+    <ark-gallery images="https://myphoto/dog.png|https://myphoto/cat.png">
     </ark-gallery>
     `
 
-    const gallery = container.querySelector("ark-gallery")
-    const images = gallery.querySelectorAll(".ark-gallery__thumbnail")
-    const selectedImage = gallery.querySelector(".ark-gallery__selected")
+    const gallery = container.querySelector('ark-gallery')
+    const thumbnails = container.querySelectorAll('[data-thumbnail]')
+    const image = gallery.querySelector('[data-image]')
 
-    images[1].click()
-    expect(selectedImage.src).toBe(gallery.imageList[1].trim())
-    images[2].click()
-    expect(selectedImage.src).toBe(gallery.imageList[2].trim())
+    thumbnails[0].click()
+    expect(image.src).toBe(gallery.imageList[0].trim())
+
+    thumbnails[1].click()
+    expect(image.src).toBe(gallery.imageList[1].trim())
   })
 
-  it("Attribute width and height are reflected on selected image", () => {
+  it('Attribute width and height are reflected on selected image', () => {
     container.innerHTML = /* html */ `
-    <ark-gallery images="https://picsum.photos/?random=1| 
-                         https://picsum.photos/?random=2| 
-                         https://picsum.photos/?random=3"
-                 width="1280px"
-                 height="720px">
+    <ark-gallery images="https://myPhoto/dog.png|https://myPhoto/cat.png"
+    width="1280px"
+    height="720px">
     </ark-gallery>
     `
 
-    const gallery = container.querySelector("ark-gallery")
-    const images = gallery.querySelectorAll(".ark-gallery__thumbnail")
-    const selectedImage = gallery.querySelector(".ark-gallery__selected")
+    const gallery = container.querySelector('ark-gallery')
+    const thumbnails = container.querySelectorAll('[data-thumbnail]')
+    const image = gallery.querySelector('[data-image]')
 
-    expect(selectedImage.getAttribute("width")).toBe("1280px")
-    expect(selectedImage.getAttribute("height")).toBe("720px")
+    expect(image.getAttribute('width')).toBe('1280px')
+    expect(image.getAttribute('height')).toBe('720px')
   })
 })
