4d0850ef22eeb44019bcb7aa090f8a4edea81ff5
jhonjimenez <jajimenez@knowark.com>
2022-10-31 10:14:12 -0500

    test files of folder base

---
+++ src/base/utils/uuid.test.js
@@ -0,0 +1,19 @@
+import { uuid } from './uuid.js'
+
+describe('Slots', () => {
+  it('groups separated by dashes', () => {
+    const id = uuid()
+    expect(id.split('-').length === 5).toBeTruthy()
+  })
+
+  it('total digits == 32', () => {
+    const id = uuid()
+
+    let digits = ''
+    id.split('-').forEach(element => {
+      digits += element
+    })
+
+    expect(digits.length === 32).toBeTruthy()
+  })
+})
